79def9d192dad86c7e644e85b26455e8cf95fe6b
Alan Osefia <amosefia@student.che.nl>
2025-08-13 15:10:18 +0200

    Structure program (bin,src,lib)

---
+++ include/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+/* 
+ Bevat de originele configuratie-instellingen voor de CHIP-8 emulator:
+ http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#2.1
+*/
+#define CHIP8_MEMORY_SIZE 4096
+#define CHIP8_WIDTH 64 
+#define CHIP8_HEIGHT 32
+#define CHIP8_WINDOW_MULTIPLIER 10
+
+#define EMULATOR_WINDOW_TITLE "CHIP-8 Emulator"
+#endif // CONFIG_H
